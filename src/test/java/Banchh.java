dd45a1a04097a1a2d4a1ed841224aa2f82890182
ZilhaKalac <zilhakalac@gmail.com>
2025-09-17 20:44:17 +0300

    ekle

--- src/test/java/Banchh.java
+++ src/test/java/Banchh.java
@@ -1,6 +1,5 @@
 public class Banchh {
     public static void main(String[] args) {
-        System.out.println("Branch ekle" +
-                "");
+        System.out.println("Branch ekle");
     }
 }
